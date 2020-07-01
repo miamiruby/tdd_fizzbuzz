67ef292eb2d14b4b220aa93fbfa6061a0777eac1
Paul Kruger <paul@miamiphp.org>
2020-07-01 15:59:44 -0400

    Create code.rb

---
+++ lib/code.rb
@@ -0,0 +1,13 @@
+def fizzBuzz(i)
+    1.upto(i).each do |n|
+        if n % 15 == 0
+            puts "FizzBuzz" 
+        elsif n % 3 == 0
+            puts "Fizz" 
+        elsif n % 5 == 0
+            puts "Buzz"
+        else  
+            puts n
+        end
+    end
+end
